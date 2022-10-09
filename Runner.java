5deda7d6e98ff17ea515274fb7c1326560b1d6cb
ComanderPotato <108785555+ComanderPotato@users.noreply.github.com>
2022-10-09 18:07:26 +1100

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -5,11 +5,6 @@ public class Runner {
 	public static void main(String[] args) {
 		try {
 			List<Token> results = LexicalAnalyser.analyse("public class Test { public static void main(String[] args) { }}");
-			// ParseTree result = SyntacticAnalyser.parse(LexicalAnalyser.analyse("public class Test { public static void main(String[] args){  }}"));
-			// System.out.println(results);
-			// List<TreeNode> children = result.getRoot().getChildren();
-			// int i = 13;
-			// System.out.print(children.get(i).getLabel() + " " + children.get(i).getToken().get().getType());
 			ParseTree tree = SyntacticAnalyser.parse(results);
 			System.out.println(tree);
 		} catch (LexicalException e) {
